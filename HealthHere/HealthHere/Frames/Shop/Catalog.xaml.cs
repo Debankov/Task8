0ced00e66253ed94cf5427295ac09894be5f113d
koval <koval@DESKTOP-MOAA9A8>
2023-11-25 18:20:57 +0700

    'showItems'

--- HealthHere/HealthHere/Frames/Shop/Catalog.xaml.cs
+++ HealthHere/HealthHere/Frames/Shop/Catalog.xaml.cs
@@ -20,9 +20,30 @@ namespace HealthHere.Frames
     /// </summary>
     public partial class Catalog : Page
     {
+        private order order = new order();
         public Catalog()
         {
             InitializeComponent();
+
+        }
+
+        public void Reload()
+        {
+            HealthHereEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+            Products.ItemsSource = HealthHereEntities.GetContext().product.Where(a => a.count != 0).ToList();
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                Reload();
+            }
+        }
+
+        public static void buy_button(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
